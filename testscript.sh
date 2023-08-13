eeded32c7a27343ee3d2b6285e58404cb9435198
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2023-08-13 01:43:29 -0700

    Add files via upload

--- testscript.sh
+++ testscript.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
+#Application list
 app1=htop
 app2=tldr
-#app3=sensors
+app3=lynis
 app4=nmap
 app5=wireshark
 app6=steam
 app7=discord
+app8=fail2ban
+app9=vim
 
+#Welcome message
 echo "Welcome to KCL App installer!"
 echo " "
 sudo apt-get update >> kcl_pack_installer.log
 
+#App install
 echo "Installing applications... "
 
 sudo apt-get install -y $app1 >> kcl_pack_installer.log
 e1=$?
 sudo apt-get install -y $app2 >> kcl_pack_installer.log
 e2=$?
-#sudo apt-get install -y $app3 >> kcl_pack_installer.log
-#e3=$?
+sudo apt-get install -y $app3 >> kcl_pack_installer.log
+e3=$?
 sudo apt-get install -y $app4 >> kcl_pack_installer.log
 e4=$?
 sudo apt-get install -y $app5 >> kcl_pack_installer.log
 e5=$?
 sudo apt-get install -y $app6 >> kcl_pack_installer.log
 e6=$?
+sudo apt-get install -y $app8 >> kcl_pack_installer.log
+e8=$?
+sudo apt-get install -y $app9 >> kcl_pack_installer.log
+e9=$?
+
 wget -q "https://discord.com/api/download?platform=linux&format=deb" >> kcl_pack_installer.log
 sudo chmod +x 'download?platform=linux&format=deb' >> kcl_pack_installer.log
 sudo dpkg -i ~/'download?platform=linux&format=deb' >> kcl_pack_installer.log
 e7=$?
 
+#exit codes & messages
 if [ $e1 -eq 0 ]
 then
     echo "$app1 successfully installed."
@@ -45,12 +56,12 @@ else
     echo "$app2 FAILED to install."
 fi
 
-#if [ $e3 -eq 0 ]
-#then
-#    echo "$app3 successfully installed."
-#else
-#    echo "$app3 FAILED to install."
-#fi
+if [ $e3 -eq 0 ]
+then
+    echo "$app3 successfully installed."
+else
+    echo "$app3 FAILED to install."
+fi
 
 if [ $e4 -eq 0 ]
 then
@@ -80,29 +91,35 @@ else
     echo "$app7 FAILED to install."
 fi
 
-echo " "
+if [ $e8 -eq 0 ]
+then
+    echo "$app8 successfully installed."
+else
+    echo "$app8 FAILED to install."
+fi
 
+if [ $e9 -eq 0 ]
+then
+    echo "$app9 successfully installed."
+else
+    echo "$app9 FAILED to install."
+fi
+
+echo " "
 sleep 3
 
+#Trash cleanup
 echo "Removing trash files..."
 currentdirectory=$(pwd)
 rm $currentdirectory/'download?platform=linux&format=deb'
 
-sleep 3
-
 echo " "
-
 sleep 3
 
+#Wireless signal cleanups
 echo "RFKill blocking wifi & bluetooth, turning wifi interface off."
 rfkill block wifi
 rfkill block bluetooth
 sudo ifconfig wlo1 down
 
-sleep 3
-echo " "
-
-echo "Turning firewall on."
-sudo ufw enable
-sudo ufw status
-sleep 3
+echo "Finished installing applications and changing wireless signals."
