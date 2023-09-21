44f3bc823aa3d28ed6eff3e5f9c9166c1953e1e8
Kclamberth <127368340+Kclamberth@users.noreply.github.com>
2023-09-20 20:27:31 -0700

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -16,7 +16,7 @@ sudo apt-get update >> /var/log/kcl_apps.log 2>&1 #updates system
 for (( line=1; line<=$e0; line++))
 do
     echo "Installing applications... ($line/$apptotal)"
-    app=$( $cat $(find / -name applist.txt 2>/dev/null) | awk -F "=" '{print $2}' | sed -n "$line"p) >> /var/log/kcl_apps.log 2>&1 )
+    app=$( $cat $(find / -name applist.txt 2>/dev/null) | awk -F "=" '{print $2}' | sed -n "$line"p >> /var/log/kcl_apps.log 2>&1 )
     sudo apt-get install -y -q $app #appends stderror AND stdout to log
     e"$line"=$?
 done
